ad718280bcf90794f752a5735cd6146aebfb93b2
StraightDroid <straightdroid@gmail.com>
2015-11-16 21:53:09 +0100

    taste of..

--- full_shooteru.mk
+++ full_shooteru.mk
@@ -21,10 +21,10 @@
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# - not on jelly bean $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from those products. Most specific first.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 PRODUCT_NAME := full_shooteru
 PRODUCT_DEVICE := shooteru
